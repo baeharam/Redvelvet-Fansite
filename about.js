9109ea36319bfab2e449a05599e480558508d593
bae haram <hisfedev@gmail.com>
2019-07-30 01:24:28 +0900

    Update original files
    Signed-off-by: bae haram <hisfedev@gmail.com>

--- about.js
+++ about.js
@@ -1,46 +1,25 @@
-let fadeInForScroll = function() {
-  let elements,windowHeight;
-  let init = function() {
-    elements = document.querySelectorAll('.hidden');
-    windowHeight = window.innerHeight;
-    addEventHandlers();
-    checkPosition();
-  }
-  let addEventHandlers = function() {
-    window.addEventListener('scroll', checkPosition);
-    window.addEventListener('resize', init);
-  }
-  let showElement = (target) => target.className = target.className.replace('hidden','fade-in');
-  let hideElement = (target) => target.className = target.className.replace('fade-in','hidden');
-
-  let checkPosition = function() {
-    for (let i = 0; i < elements.length; i++) {
-      let positionInfo = elements[i].getBoundingClientRect();
-      let positionFromTop = positionInfo.top;
-      let elementHeight = positionInfo.height;
-
-      if (positionFromTop - windowHeight <= 0) { 
-        showElement(elements[i]); 
+const initializeScrollEffect = function() {
+  const intersectionObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if(entry.intersectionRatio > 0) {
+        entry.target.className = entry.target.className.replace('hidden','fadeIn');
+      } else {
+        entry.target.className = entry.target.className.replace('fadeIn','hidden');
       }
-      else if ((positionFromTop - windowHeight > 1) || (positionFromTop + elementHeight < 0)) {
-        hideElement(elements[i]);
-      }
-    }
-  }
-  return { init: init };
-};
+    });
+  });
+  const fadeInElemes = document.querySelectorAll('.hidden');
+  fadeInElemes.forEach((fadeInElem) => intersectionObserver.observe(fadeInElem));
+}
 
-let initializeMenu = function() {
-  let main = document.querySelector('main');
-  let header = document.querySelector('header');
-  main.style.paddingTop = header.offsetHeight + 'px';
+const initializeFullpageEffect = function() {
+  new fullpage('#fullpage', {
+    autoScrolling: true,
+  });
+  fullpage_api.setAllowScrolling(false);
 }
 
 window.onload = () => {
-  fadeInForScroll().init();
-  initializeMenu();
-}
-
-window.onresize = () => {
-  initializeMenu();
+  initializeScrollEffect();
+  initializeFullpageEffect();
 }
