3550940825d1f0beb95d052250be60dc517691f3
bae haram <hisfedev@gmail.com>
2019-08-04 00:00:42 +0900

    Test background music of YouTube
    Signed-off-by: bae haram <hisfedev@gmail.com>

--- js/discography.js
+++ js/discography.js
@@ -4,6 +4,35 @@ URL: https://codepen.io/knyttneve/pen/bgvmma/
 I changed jQuery source code into pure javascript
 */
 
+let player;
+
+const playMusicByYoutube = function(youtubeID) {
+
+    if(player) {
+        console.log('Exist',youtubeID);
+        player.loadVideoById(youtubeID);
+        return;
+    }
+
+    const loadPlayer = function() {
+        player = new YT.Player('timeline__player-js', {
+            videoId: youtubeID,
+            loop: true,
+            events: {
+                onReady: (e) => e.target.playVideo()
+            }
+        });
+    }
+
+    if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
+        window.onYouTubePlayerAPIReady = function() {
+            loadPlayer();
+        };
+    } else {
+        loadPlayer();
+    }
+}
+
 const initializeTimeline = function() {
     const timeline = document.getElementById('timeline-js');
     const selectors = {
@@ -18,6 +47,7 @@ const initializeTimeline = function() {
     }
     selectors.items[0].classList.add(selectors.activeClass);
     selectors.id.style.backgroundImage = `url(${selectors.imgs[0].src})`;
+    // playMusicByYoutube(selectors.items[0].dataset.id);
     let itemLength = selectors.items.length;
     window.addEventListener('scroll', function(){
         let max,min;
@@ -29,13 +59,17 @@ const initializeTimeline = function() {
                 selectors.removeAllActive();
                 selectors.id.style.backgroundImage = `url(${selectors.imgs[itemLength-1].src})`;
                 selectors.items[itemLength-1].classList.add(selectors.activeClass);
+                playMusicByYoutube(selectors.items[itemLength-1].dataset.id);
             } else if(pos <= max-40 && pos >= min){
                 let src = document.querySelector('.timeline__item--active .timeline__img').src;
                 selectors.id.style.backgroundImage = `url(${src})`;
                 selectors.removeAllActive();
                 item.classList.add(selectors.activeClass);
+                playMusicByYoutube(item.dataset.id);
             }
         });
     });
 }
+
+
 initializeTimeline();
