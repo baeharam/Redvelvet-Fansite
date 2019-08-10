5ffe8a5e59801c861b898d39f5bd1fcbd90faa3e
bae haram <hisfedev@gmail.com>
2019-08-10 16:07:55 +0900

    Add backBtn work
    Signed-off-by: bae haram <hisfedev@gmail.com>

--- js/photo.js
+++ js/photo.js
@@ -11,10 +11,38 @@ const CLASSES = (function makeCommonClasses() {
   };
 }());
 
+const initObserverForBackground = function initObserver() {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        document.getElementsByTagName('body')[0].style.backgroundColor = entry.target.dataset.color;
+      }
+    });
+  });
+  CLASSES.SWIPER_IMGS.forEach(swiperImg => observer.observe(swiperImg));
+};
+
+const initFullpage = function initFullpage() {
+  new fullpage('#fullpage', {
+    autoScrolling: true,
+    afterLoad(_origin, destination) {
+      CLASSES.SWIPER_IMGLEFTS[destination.index].classList.add('left-rotate');
+      CLASSES.SWIPER_IMGRIGHTS[destination.index].classList.add('right-rotate');
+    },
+    onLeave(origin) {
+      CLASSES.SWIPER_IMGLEFTS[origin.index].classList.remove('left-rotate');
+      CLASSES.SWIPER_IMGRIGHTS[origin.index].classList.remove('right-rotate');
+    },
+  });
+  fullpage_api.setAllowScrolling(true);
+};
+
 const initAnimation = function initAnimation() {
   const swiperTitles = document.querySelectorAll('.swiper__title');
   const swiperContentsInsides = document.querySelectorAll('.swiper__contents__inside');
   const sections = document.querySelectorAll('.section');
+  const backBtn = document.getElementById('backBtn-js');
+  const header = document.getElementById('header-js');
 
   const addHoverOnEventToImg = function addHoverOnEventToImg() {
     CLASSES.SWIPER_IMGS.forEach((swiperImg, index) => swiperImg.addEventListener('mouseenter', () => {
@@ -52,29 +80,72 @@ const initAnimation = function initAnimation() {
     }));
   };
 
-  const runClickAnimation = function runClickAnimation(swiperImg, index) {
+  const showHeader = function showHeader() {
+    header.classList.remove('hide');
+    header.classList.add('show');
+  };
+
+  const hideHeader = function hideHeader() {
+    header.classList.remove('show');
+    header.classList.add('hide');
+  };
+
+  const showBackBtn = function showBackBtn() {
+    backBtn.classList.add('show');
+  };
+
+  const hideBackBtn = function hideBackBtn() {
+    backBtn.classList.remove('show');
+  };
+
+  const forwardClickAnimation = function forwardClickAnimation(swiperImg, index) {
+    CLASSES.SWIPER_IMGLEFTS[index].classList.remove('left-gone-backward');
+    CLASSES.SWIPER_IMGRIGHTS[index].classList.remove('right-gone-backward');
     CLASSES.SWIPER_IMGLEFTS[index].classList.add('left-gone');
     CLASSES.SWIPER_IMGRIGHTS[index].classList.add('right-gone');
+    swiperTitles[index].classList.remove('move-up-backward');
+    swiperImg.classList.remove('move-down-backward');
     swiperTitles[index].classList.add('move-up');
     swiperImg.classList.add('move-down');
     sections.forEach((section, sectionIndex) => {
       if (sectionIndex !== index) {
-        section.style.display = 'none';
+        section.classList.add('hide');
       }
     });
-    swiperContentsInsides[index].classList.add('show');
+    swiperContentsInsides[index].classList.add('show-contents');
   };
 
-  const removeHeader = function removeHeader() {
-    document.getElementById('header-js').classList.add('hide');
+  const backwardClickAnimation = function backwardClickAnimation() {
+    // eslint-disable-next-line prefer-destructuring
+    const index = fullpage_api.getActiveSection().index;
+    CLASSES.SWIPER_IMGLEFTS[index].classList.remove('left-gone');
+    CLASSES.SWIPER_IMGRIGHTS[index].classList.remove('right-gone');
+    CLASSES.SWIPER_IMGLEFTS[index].classList.add('left-gone-backward');
+    CLASSES.SWIPER_IMGRIGHTS[index].classList.add('right-gone-backward');
+    swiperTitles[index].classList.remove('move-up');
+    CLASSES.SWIPER_IMGS[index].classList.remove('move-down');
+    swiperTitles[index].classList.add('move-up-backward');
+    CLASSES.SWIPER_IMGS[index].classList.add('move-down-backward');
+    document.querySelector('.show-contents').classList.remove('show-contents');
+    sections.forEach(section => section.classList.remove('hide'));
   };
 
   const addClickEventToImg = () => {
     CLASSES.SWIPER_IMGS.forEach((swiperImg, index) => swiperImg.addEventListener('click', () => {
-      runClickAnimation(swiperImg, index);
-      removeHeader();
-      fullpage_api.destroy();
+      forwardClickAnimation(swiperImg, index);
+      hideHeader();
+      showBackBtn();
+      fullpage_api.setAutoScrolling(false);
     }));
+  };
+
+  const addClickEventToBackBtn = function addClickEventToBackBtn() {
+    backBtn.addEventListener('click', () => {
+      backwardClickAnimation();
+      showHeader();
+      hideBackBtn();
+      fullpage_api.setAutoScrolling(true);
+    });
   };
 
   const addNoRebuildEvent = () => {
@@ -92,32 +163,7 @@ const initAnimation = function initAnimation() {
   addHoverOffEventToTitle();
   addClickEventToImg();
   addNoRebuildEvent();
-};
-
-const initObserverForBackground = function initObserver() {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        document.getElementsByTagName('body')[0].style.backgroundColor = entry.target.dataset.color;
-      }
-    });
-  });
-  CLASSES.SWIPER_IMGS.forEach(swiperImg => observer.observe(swiperImg));
-};
-
-const initFullpage = function initFullpage() {
-  new fullpage('#fullpage', {
-    autoScrolling: true,
-    afterLoad(_origin, destination) {
-      CLASSES.SWIPER_IMGLEFTS[destination.index].classList.add('left-rotate');
-      CLASSES.SWIPER_IMGRIGHTS[destination.index].classList.add('right-rotate');
-    },
-    onLeave(origin) {
-      CLASSES.SWIPER_IMGLEFTS[origin.index].classList.remove('left-rotate');
-      CLASSES.SWIPER_IMGRIGHTS[origin.index].classList.remove('right-rotate');
-    },
-  });
-  fullpage_api.setAllowScrolling(true);
+  addClickEventToBackBtn();
 };
 
 window.onload = () => {
