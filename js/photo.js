b937d0ceec13c759c93b3b1da6fcf94ebb1b73f1
bae haram <hisfedev@gmail.com>
2019-08-10 13:26:08 +0900

    Install stylelint
    Signed-off-by: bae haram <hisfedev@gmail.com>

--- js/photo.js
+++ js/photo.js
@@ -3,54 +3,59 @@
 /* eslint-disable no-undef */
 /* 위 사항들에 대한 무시는 fullpage.js의 것들을 사용하기 때문이다. */
 
-const initializeAnimation = function initializeAnimation() {
-  const swiperImgs = document.querySelectorAll('.swiper__img');
+const CLASSES = (function makeCommonClasses() {
+  return {
+    SWIPER_IMGS: document.querySelectorAll('.swiper__img'),
+    SWIPER_IMGLEFTS: document.querySelectorAll('.swiper__img__left'),
+    SWIPER_IMGRIGHTS: document.querySelectorAll('.swiper__img__right'),
+  };
+}());
+
+const initAnimation = function initAnimation() {
   const swiperTitles = document.querySelectorAll('.swiper__title');
-  const swiperImgLefts = document.querySelectorAll('.swiper__img__left');
-  const swiperImgRights = document.querySelectorAll('.swiper__img__right');
   const swiperContentsInsides = document.querySelectorAll('.swiper__contents__inside');
   const sections = document.querySelectorAll('.section');
 
   const addHoverOnEventToImg = function addHoverOnEventToImg() {
-    swiperImgs.forEach((swiperImg, index) => swiperImg.addEventListener('mouseenter', () => {
+    CLASSES.SWIPER_IMGS.forEach((swiperImg, index) => swiperImg.addEventListener('mouseenter', () => {
       swiperImg.classList.add('hover-up');
       swiperTitles[index].classList.add('hover-up');
-      swiperImgLefts[index].classList.add('hover-hide');
-      swiperImgRights[index].classList.add('hover-hide');
+      CLASSES.SWIPER_IMGLEFTS[index].classList.add('hover-hide');
+      CLASSES.SWIPER_IMGRIGHTS[index].classList.add('hover-hide');
     }));
   };
 
   const addHoverOnEventToTItle = function addHoverOnEventToTItle() {
     swiperTitles.forEach((swiperTitle, index) => swiperTitle.addEventListener('mouseenter', () => {
       swiperTitle.classList.add('hover-up');
-      swiperImgs[index].classList.add('hover-up');
-      swiperImgLefts[index].classList.add('hover-hide');
-      swiperImgRights[index].classList.add('hover-hide');
+      CLASSES.SWIPER_IMGS[index].classList.add('hover-up');
+      CLASSES.SWIPER_IMGLEFTS[index].classList.add('hover-hide');
+      CLASSES.SWIPER_IMGRIGHTS[index].classList.add('hover-hide');
     }));
   };
 
   const addHoverOffEventToImg = function addHoverOffEventToImg() {
-    swiperImgs.forEach((swiperImg, index) => swiperImg.addEventListener('mouseleave', () => {
+    CLASSES.SWIPER_IMGS.forEach((swiperImg, index) => swiperImg.addEventListener('mouseleave', () => {
       swiperImg.classList.remove('hover-up');
       swiperTitles[index].classList.remove('hover-up');
-      swiperImgLefts[index].classList.remove('hover-hide');
-      swiperImgRights[index].classList.remove('hover-hide');
+      CLASSES.SWIPER_IMGLEFTS[index].classList.remove('hover-hide');
+      CLASSES.SWIPER_IMGRIGHTS[index].classList.remove('hover-hide');
     }));
   };
 
   const addHoverOffEventToTitle = function addHoverOffEventToTitle() {
     swiperTitles.forEach((swiperTitle, index) => swiperTitle.addEventListener('mouseleave', () => {
       swiperTitle.classList.remove('hover-up');
-      swiperImgs[index].classList.remove('hover-up');
-      swiperImgLefts[index].classList.remove('hover-hide');
-      swiperImgRights[index].classList.remove('hover-hide');
+      CLASSES.SWIPER_IMGS[index].classList.remove('hover-up');
+      CLASSES.SWIPER_IMGLEFTS[index].classList.remove('hover-hide');
+      CLASSES.SWIPER_IMGRIGHTS[index].classList.remove('hover-hide');
     }));
   };
 
   const addClickEventToImg = () => {
-    swiperImgs.forEach((swiperImg, index) => swiperImg.addEventListener('click', () => {
-      swiperImgLefts[index].classList.add('left-gone');
-      swiperImgRights[index].classList.add('right-gone');
+    CLASSES.SWIPER_IMGS.forEach((swiperImg, index) => swiperImg.addEventListener('click', () => {
+      CLASSES.SWIPER_IMGLEFTS[index].classList.add('left-gone');
+      CLASSES.SWIPER_IMGRIGHTS[index].classList.add('right-gone');
       swiperTitles[index].classList.add('move-up');
       swiperImg.classList.add('move-down');
       sections.forEach((section, sectionIndex) => {
@@ -67,7 +72,6 @@ const initializeAnimation = function initializeAnimation() {
     window.addEventListener('resize', () => {
       if (matchMedia('(max-width: 768px)').matches) {
         const moveDownElem = document.querySelector('.move-down');
-        // moveDownElem.classList.remove('move-down');
         moveDownElem.classList.add('no-rebuild');
       }
     });
@@ -81,14 +85,34 @@ const initializeAnimation = function initializeAnimation() {
   addNoRebuildEvent();
 };
 
-const initializeFullpage = function initializeFullpage() {
+const initObserverForBackground = function initObserver() {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        document.getElementsByTagName('body')[0].style.backgroundColor = entry.target.dataset.color;
+      }
+    });
+  });
+  CLASSES.SWIPER_IMGS.forEach(swiperImg => observer.observe(swiperImg));
+};
+
+const initFullpage = function initFullpage() {
   new fullpage('#fullpage', {
     autoScrolling: true,
+    afterLoad(_origin, destination) {
+      CLASSES.SWIPER_IMGLEFTS[destination.index].classList.add('left-rotate');
+      CLASSES.SWIPER_IMGRIGHTS[destination.index].classList.add('right-rotate');
+    },
+    onLeave(origin) {
+      CLASSES.SWIPER_IMGLEFTS[origin.index].classList.remove('left-rotate');
+      CLASSES.SWIPER_IMGRIGHTS[origin.index].classList.remove('right-rotate');
+    },
   });
   fullpage_api.setAllowScrolling(true);
 };
 
 window.onload = () => {
-  initializeAnimation();
-  initializeFullpage();
+  initAnimation();
+  initObserverForBackground();
+  initFullpage();
 };
