23fa40bbea163bb8f72d3576e8223084ea9acc9b
bae haram <hisfedev@gmail.com>
2019-08-15 19:13:51 +0900

    Make video page
    Signed-off-by: bae haram <hisfedev@gmail.com>

--- js/video.js
+++ js/video.js
@@ -1,37 +1,73 @@
-const initializeCarousel = function initializeCarousel() {
-  const slidesContainer = document.querySelector('.carousel-slides');
-  const slides = [].slice.call(slidesContainer.children);
+function moveToSelected(element) {
+  let selected;
+  if (element === 'next') {
+    selected = document.querySelector('.selected').nextElementSibling;
+  } else if (element === 'prev') {
+    selected = document.querySelector('.selected').previousElementSibling;
+  } else {
+    selected = element;
+  }
 
-  const leftBtn = document.querySelector('.button-play--left');
-  const rightBtn = document.querySelector('.button-play--right');
+  let next = selected ? selected.nextElementSibling : null;
+  let prev = selected ? selected.previousElementSibling : null;
 
-  const slideWidth = slides[0].getBoundingClientRect().width;
-  const setSlidePosition = (slide, index) => {
-    slide.style.left = `${slideWidth * index}px`;
-  };
-  slides.forEach(setSlidePosition);
+  if (selected) {
+    selected.classList = '';
+    selected.classList.add('carousel__contents');
+    selected.classList.add('selected');
+  }
+  if (prev) {
+    prev.classList = '';
+    prev.classList.add('carousel__contents');
+    prev.classList.add('prev');
+  }
+  if (next) {
+    next.classList = '';
+    next.classList.add('carousel__contents');
+    next.classList.add('next');
+  }
 
-  const moveVideo = (currentVideo, targetVideo, moveValue) => {
-    slidesContainer.style.transform = `translateX(${moveValue})`;
-    currentVideo.classList.remove('active');
-    targetVideo.classList.add('active');
-  };
+  while (next && next.nextElementSibling) {
+    next.nextElementSibling.classList = '';
+    next.nextElementSibling.classList.add('carousel__contents');
+    next.nextElementSibling.classList.add('hide');
+    next = next.nextElementSibling;
+  }
 
-  leftBtn.addEventListener('click', () => {
-    const currentVideo = document.querySelector('.active');
-    const prevVideo = currentVideo.previousElementSibling;
-    if (prevVideo) {
-      moveVideo(currentVideo, prevVideo, prevVideo.style.left);
+  while (prev && prev.previousElementSibling) {
+    prev.previousElementSibling.classList = '';
+    prev.previousElementSibling.classList.add('carousel__contents');
+    prev.previousElementSibling.classList.add('hide');
+    prev = prev.previousElementSibling;
+  }
+}
+
+document.addEventListener('keydown', (e) => {
+  switch (e.which) {
+    case 37:
+      moveToSelected('prev');
+      break;
+    case 39:
+      moveToSelected('next');
+      break;
+    default:
+  }
+  e.preventDefault();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+  moveToSelected('prev');
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+  moveToSelected('next');
+});
+
+document.querySelectorAll('.video__player').forEach((videoElem) => {
+  videoElem.addEventListener('mouseenter', () => {
+    const src = videoElem.getAttribute('src');
+    if (!src.includes('autoplay')) {
+      videoElem.setAttribute('src', `${src}&autoplay=1`);
     }
   });
-
-  rightBtn.addEventListener('click', () => {
-    const currentVideo = document.querySelector('.active');
-    const nextVideo = currentVideo.nextElementSibling;
-    if (nextVideo) {
-      moveVideo(currentVideo, nextVideo, `-${nextVideo.style.left}`);
-    }
-  });
-};
-
-initializeCarousel();
+});
