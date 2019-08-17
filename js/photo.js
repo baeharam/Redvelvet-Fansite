c3f63884540c43f2b3db0ea56f7803abb7723f93
bae haram <hisfedev@gmail.com>
2019-08-17 15:19:23 +0900

    Change whole layout of photo page
    Signed-off-by: bae haram <hisfedev@gmail.com>

--- js/photo.js
+++ js/photo.js
@@ -1,140 +1,66 @@
-/* eslint-disable new-cap */
-/* eslint-disable no-new */
-/* eslint-disable no-undef */
-/* 위 사항들에 대한 무시는 fullpage.js의 것들을 사용하기 때문이다. */
+const initMasonry = function initMasonry() {
+  const grid = document.querySelector('.grid');
+  let msnry;
 
-const initObserverForBackground = function initObserver() {
-  let observer;
-  const imgs = document.querySelectorAll('.swiper__img');
-  const mainImgs = document.querySelectorAll('.swiper__contents__main');
-  const leftImgs = document.querySelectorAll('.swiper__img__left');
-  const rightImgs = document.querySelectorAll('.swiper__img__right');
-
-  const makeObserverInstance = function makeObserverInstance() {
-    observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          if (entry.target.classList.contains('swiper__img')) {
-            // document.getElementsByTagName('body')[0].style.backgroundColor = entry.target.dataset.color;
-          } else if (entry.target.classList.contains('swiper__img__left')
-            || entry.target.classList.contains('swiper__img__right')
-            || entry.target.classList.contains('swiper__contents__main')) {
-            entry.target.style.backgroundImage = `url(${entry.target.dataset.src})`;
-          }
-        }
-      });
+  const makeMasonry = function makeMasonry() {
+    // eslint-disable-next-line no-undef
+    msnry = new Masonry(grid, {
+      itemSelector: '.grid__item',
+      columnWidth: '.grid__sizer',
+      gutter: '.gutter-sizer',
+      percentPosition: true,
     });
   };
 
-  const observeElements = function observElements() {
-    imgs.forEach(swiperImg => observer.observe(swiperImg));
-    leftImgs.forEach(swiperLeftImg => observer.observe(swiperLeftImg));
-    rightImgs.forEach(swiperRightImg => observer.observe(swiperRightImg));
-    mainImgs.forEach(swiperContentsMain => observer.observe(swiperContentsMain));
+  const initReiszeEvent = function initReiszeEvent() {
+    window.addEventListener('resize', () => {
+      msnry.destroy();
+      makeMasonry();
+    });
   };
 
-  makeObserverInstance();
-  observeElements();
+  makeMasonry();
+  initReiszeEvent();
 };
 
-const initFullpage = function initFullpage() {
-  new fullpage('#fullpage', {
-    anchors: ['irene', 'seulgi', 'wendy', 'yeri', 'joy'],
-    autoScrolling: true,
-    afterLoad(_origin, destination) {
-      destination.item.classList.add('load');
-    },
-    onLeave(origin) {
-      origin.item.classList.remove('load');
-    },
+const initLazyLoading = function initLazyLoading() {
+  const smallImgs = document.querySelectorAll('.placeholder__img');
+
+  smallImgs.forEach((smallImg) => {
+    const img = new Image();
+    img.src = smallImg.src;
+    img.onload = () => smallImg.classList.add('loaded');
   });
-  fullpage_api.setAllowScrolling(true);
 };
 
-const initAnimation = function initAnimation() {
-  const header = document.getElementById('header-js');
-  const backBtn = document.getElementById('backBtn-js');
-  const sections = document.querySelectorAll('.section');
+const initObserver = function initObserver(io) {
+  const gridItems = document.querySelectorAll('.grid__item');
+  const placeholders = document.querySelectorAll('.placeholder');
+  gridItems.forEach(gridItem => io.observe(gridItem));
+  placeholders.forEach(placeholder => io.observe(placeholder));
+};
 
-  const setHoverEffect = function setHoverEffect() {
-    sections.forEach((section) => {
-      const mouseenterHandler = () => { section.classList.add('hover'); };
-      const mouseleaveHandler = () => { section.classList.remove('hover'); };
-      section.querySelectorAll('.swiper__title, .swiper__contents__main, .swiper__img__left, .swiper__img__right').forEach((el) => {
-        el.addEventListener('mouseenter', mouseenterHandler);
-        el.addEventListener('mouseleave', mouseleaveHandler);
-      });
-    });
-  };
-
-  const forwardClickAnimation = function forwardClickAnimation(_swiperImg, index) {
-    header.classList.remove('out');
-    header.classList.add('in');
-    backBtn.classList.remove('out');
-    backBtn.classList.add('in');
-    sections[index].classList.remove('out');
-    sections[index].classList.add('in');
-    sections.forEach((section, sectionIndex) => {
-      if (sectionIndex !== index) {
-        section.classList.add('hide');
+const initScrollBehaviors = function initScrollBehaviors() {
+  const io = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (entry.target.className === 'grid__item') {
+          entry.target.classList.add('grid__item--scale');
+        } else {
+          const originalImg = new Image();
+          originalImg.onload = () => originalImg.classList.add('loaded');
+          originalImg.src = entry.target.dataset.large;
+          entry.target.firstElementChild.classList.remove('loaded');
+          entry.target.appendChild(originalImg);
+        }
       }
     });
-  };
-
-  const backwardClickAnimation = function backwardClickAnimation(index) {
-    // eslint-disable-next-line prefer-destructuring
-    // const index = fullpage_api.getActiveSection().index;
-    const activateAnimation = function animate() {
-      header.classList.remove('in');
-      header.classList.add('out');
-      backBtn.classList.remove('in');
-      backBtn.classList.add('out');
-      sections[index].classList.remove('in');
-      sections[index].classList.add('out');
-      sections.forEach((section, sectionIndex) => {
-        if (sectionIndex !== index) {
-          section.classList.remove('hide');
-        }
-      });
-    };
-
-    const replaceOutToLoad = function replaceOutToLoad() {
-      document.querySelectorAll('.swiper__img')[index].addEventListener('animationend', () => {
-        sections[index].classList.replace('out', 'load');
-      });
-    };
-
-    activateAnimation();
-    replaceOutToLoad();
-  };
-
-  let currentIndex;
-  const addClickEventToSwiper = () => {
-    document.querySelectorAll('.swiper__img').forEach((swiperImg, index) => swiperImg.addEventListener('click',
-      () => {
-        currentIndex = index;
-        forwardClickAnimation(swiperImg, index);
-        fullpage_api.setAutoScrolling(false);
-        fullpage_api.setLockAnchors(true);
-      }));
-  };
-
-  const addClickEventToBackBtn = function addClickEventToBackBtn() {
-    backBtn.addEventListener('click', () => {
-      backwardClickAnimation(currentIndex);
-      fullpage_api.moveTo(currentIndex + 1);
-      fullpage_api.setAutoScrolling(true);
-      fullpage_api.setLockAnchors(false);
-    });
-  };
-
-  setHoverEffect();
-  addClickEventToSwiper();
-  addClickEventToBackBtn();
+  });
+  initObserver(io);
 };
 
 window.onload = () => {
-  initAnimation();
-  initObserverForBackground();
-  initFullpage();
+  initMasonry();
+  initLazyLoading();
+  initScrollBehaviors();
 };
