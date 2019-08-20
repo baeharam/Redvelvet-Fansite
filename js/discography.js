a73e9a49c9f99993d39fb337f7abdbabc8c36b45
bae haram <hisfedev@gmail.com>
2019-08-20 22:42:36 +0900

    Add safari specific code
    Signed-off-by: bae haram <hisfedev@gmail.com>

--- js/discography.js
+++ js/discography.js
@@ -49,7 +49,7 @@ const initScrollEvent = function initScrollEvent() {
   playMusicByYoutube(timelineItems[0].dataset.id);
 
   window.addEventListener('scroll', () => {
-    const pos = document.documentElement.scrollTop;
+    const pos = document.body.scrollTop || document.documentElement.scrollTop;
     timelineItems.forEach((item) => {
       const minPos = item.offsetTop;
       const maxPos = minPos + item.offsetHeight;
