91f67f544923e64607a8ed7456ccf8b01ea27e46
bae haram <hisfedev@gmail.com>
2019-10-27 15:03:28 +0900

    Reset configuration
    Signed-off-by: bae haram <hisfedev@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,27 @@
 const path = require('path');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const Jarvis = require('webpack-jarvis');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js'],
+  },
   entry: {
-    index: './js/index.js',
-    about: './js/about.js',
-    photo: './js/photo.js',
-    discography: './js/discography.js',
-    video: './js/video.js',
+    index: './js/index',
+    about: './js/about',
+    photo: './js/photo',
+    discography: './js/discography',
+    video: './js/video',
   },
   output: {
-    filename: '[name].[chunkhash:4].js',
+    filename: '[name].js',
     path: path.join(__dirname, 'dist'),
     publicPath: './',
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-  },
-  optimization: {
-    minimizer: [
-      new OptimizeCssAssetsPlugin({}),
-    ],
-  },
   plugins: [
-    new MiniCssExtractPlugin({ filename: '[name].[contenthash:4].css' }),
-    new Jarvis({ port: 1337 }),
-    new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      chunks: ['index'],
-      template: './html/index.html',
-      filename: 'index.html',
-    }),
-    new HtmlWebpackPlugin({
-      chunks: ['about'],
-      template: './html/about.html',
-      filename: 'about.html',
-    }),
-    new HtmlWebpackPlugin({
-      chunks: ['photo'],
-      template: './html/photo.html',
-      filename: 'photo.html',
-    }),
-    new HtmlWebpackPlugin({
-      chunks: ['discography'],
-      template: './html/discography.html',
-      filename: 'discography.html',
-    }),
-    new HtmlWebpackPlugin({
-      chunks: ['video'],
-      template: './html/video.html',
-      filename: 'video.html',
-    }),
   ],
   module: {
     rules: [
@@ -73,7 +35,7 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+        test: /\.(jpg|jpeg|png|svg|woff)$/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]?[contenthash:4]',
@@ -83,7 +45,10 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        use: ['babel-loader'],
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
         exclude: /node_modules/,
       },
     ],
